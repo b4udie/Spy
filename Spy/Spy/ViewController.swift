9bf2e74f25c3b922f92c07160efabe8b071d5d5f
Val Bratkevich <v.bratkevych@andersenlab.com>
2021-01-30 23:30:24 +0200

    Initial commit

---
+++ Spy/Spy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spy
+//
+//  Created by Val Bratkevich on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
